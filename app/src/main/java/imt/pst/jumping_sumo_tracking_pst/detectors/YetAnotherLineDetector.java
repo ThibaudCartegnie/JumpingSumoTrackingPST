5b5e0d56d8076115e789be8ac44cf3f5b8f8c7e3
Natu181 <thibaud.cartegnie@gmail.com>
2017-12-22 16:12:17 +0100

    yeay finished! line tracking works fine

--- app/src/main/java/imt/pst/jumping_sumo_tracking_pst/detectors/YetAnotherLineDetector.java
+++ app/src/main/java/imt/pst/jumping_sumo_tracking_pst/detectors/YetAnotherLineDetector.java
@@ -11,8 +11,6 @@ import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
-import java.util.concurrent.ArrayBlockingQueue;
-
 import imt.pst.jumping_sumo_tracking_pst.drone.JumpingSumo;
 
 /**
@@ -26,9 +24,9 @@ public class YetAnotherLineDetector implements Detector {
     private Rect roi_rect;
     private JumpingSumo mDrone = null;
 
-    private ArrayBlockingQueue<Integer> middle_pos;
 
     private String TAG = "YALD";
+    private int HEIGHT_ROI = 10;
 
     /**
      * Default constructor for the Jumping Sumo Parrot Drone
@@ -48,10 +46,8 @@ public class YetAnotherLineDetector implements Detector {
         mGray = new Mat(height, width, CvType.CV_8UC1);
 
         // height and width are reverted because of reasons I suppose
-        roi_rect = new Rect(0, width - 10, height, 1);
+        roi_rect = new Rect(0, width - 10, height, HEIGHT_ROI);
         thresholded_roi = new Mat(1, width, CvType.CV_8UC1);
-
-        middle_pos = new ArrayBlockingQueue<Integer>(20, true);
     }
 
     /**
@@ -68,47 +64,46 @@ public class YetAnotherLineDetector implements Detector {
 
         Mat roi = new Mat(mGray, roi_rect);
         Core.inRange(roi, new Scalar(0), new Scalar(100), thresholded_roi);
-        int nb_black_pixels = Core.countNonZero(thresholded_roi);
 
-        int nb_black_counted = 0, pos = 0;
-        while (nb_black_counted <= nb_black_pixels / 2 && pos < thresholded_roi.cols()) {
-            if (thresholded_roi.get(0, pos)[0] > 0) {
-                nb_black_counted++;
+        int[] pos = new int[HEIGHT_ROI];
+        int[] looked_lines = new int[]{0,HEIGHT_ROI-1};
+
+        for(int nb_row : looked_lines){
+            int nb_black_pixels = 0, counted_black_pixels = 0;
+
+            Mat row = thresholded_roi.row(nb_row);
+            nb_black_pixels = Core.countNonZero(row);
+            for(int i = 0; i < row.cols(); i++){
+                pos[nb_row]++;
+                if(row.get(0, i)[0] > 0){
+                    counted_black_pixels++;
+                }
+                if(nb_black_pixels/2 < counted_black_pixels){
+                    pos[nb_row] = i;
+                    break;
+                }
             }
-            pos++;
         }
 
-        //at first, fill the queue
-        if (middle_pos.remainingCapacity() != 0) {
-            middle_pos.add(pos);
-            Imgproc.rectangle(mRgb, roi_rect.tl(), roi_rect.br(), new Scalar(255, 0, 0));
-            Bitmap out = originalFrame.copy(originalFrame.getConfig(), originalFrame.isMutable());
-            Utils.matToBitmap(mRgb, out);
-            return out;
-        }
-
-        middle_pos.poll();
-        middle_pos.add(pos);
-
-        int pos_to_follow = ((Integer) middle_pos.toArray()[0] + (Integer) middle_pos.toArray()[middle_pos.size() - 1]) / 2;
-        int middle = 480 / 2;
-        if (pos == 0) {
+        int pos_to_follow = (pos[0] + pos[HEIGHT_ROI-1]) /2;
+        int middle = 640 / 2;
+        if (pos_to_follow < 20 || pos_to_follow > 620) {
             mDrone.setSpeed(JumpingSumo.NULL_SPEED);
             mDrone.setTurn(JumpingSumo.NO_TURN);
             mDrone.setFlag(JumpingSumo.FLAG_DO_NOT_RUN);
-        } else if (pos_to_follow > middle + 30) {
+        } else if (pos_to_follow > middle + 50) {
             //turn right
             mDrone.setSpeed((byte) (5 * JumpingSumo.FORWARD_SPEED));
             mDrone.setTurn((byte) (5 * JumpingSumo.RIGHT_TURN));
             mDrone.setFlag(JumpingSumo.FLAG_RUN);
-        } else if (pos_to_follow < middle - 30) {
+        } else if (pos_to_follow < middle - 50) {
             //turn left
             mDrone.setSpeed((byte) (5 * JumpingSumo.FORWARD_SPEED));
             mDrone.setTurn((byte) (5 * JumpingSumo.LEFT_TURN));
             mDrone.setFlag(JumpingSumo.FLAG_RUN);
         } else {
             //go straight
-            mDrone.setSpeed((byte) (25 * JumpingSumo.FORWARD_SPEED));
+            mDrone.setSpeed((byte) (20 * JumpingSumo.FORWARD_SPEED));
             mDrone.setTurn(JumpingSumo.NO_TURN);
             mDrone.setFlag(JumpingSumo.FLAG_RUN);
         }
